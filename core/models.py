798501f7a765cb50723e68a944fa5250f62b7ec0
Renato Cesar Arantes <renato.arantes@hegv.cesp.ses.rj.gov.br>
2023-08-08 21:05:02 -0300

    Criando tabela agenda - DIO

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Evento(models.Model):
+    título = models.CharField(max_length=100)
+    descrição = models.TextField(blank=True, null=True)
+    data_evento = models.DateField(verbose_name='Data do Evento')
+    data_criação = models.DateTimeField(auto_now_add=True, verbose_name='Data de Criação')
+    usuário = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.título
